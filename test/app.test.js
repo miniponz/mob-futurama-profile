1151080a42b4d314b545ae50004b033ce0fd1cac
Tommy Tran <tranttommy95@gmail.com>
2019-04-15 16:37:13 -0700

    api call test passed

--- test/app.test.js
+++ test/app.test.js
@@ -1,27 +1,36 @@
 const request = require('supertest');
 const app = require('../lib/app.js');
 const Profile = require('../lib/models/Profile.js');
+const getQuote = require('../lib/services/futuramaApi');
 
-describe('testing routes', () => {
-  afterEach(() => {
-    return Profile.drop();
-  });
-  it('creates a new profile', () => {
-    return request(app)
-      .post('/profiles')
-      .send({
-        name: 'bonnie',
-        favoriteCharacter: 'Bender'
-      })
-      .then(res => {
-        expect(res.body).toEqual({
-          name: 'bonnie',
-          favoriteCharacter: 'Bender',
-          tagline: expect.any(String),
-          _id: expect.any(String)
-        });
+describe('api call', () => {
+  it('gets a quote by character', () => {
+    return getQuote('bender')
+      .then(quote => {
+        expect(quote).toEqual(expect.any(String));
       });
   });
-
-  
 });
+
+
+// describe('testing routes', () => {
+//   afterEach(() => {
+//     return Profile.drop();
+//   });
+//   it('creates a new profile', () => {
+//     return request(app)
+//       .post('/profiles')
+//       .send({
+//         name: 'bonnie',
+//         favoriteCharacter: 'Bender'
+//       })
+//       .then(res => {
+//         expect(res.body).toEqual({
+//           name: 'bonnie',
+//           favoriteCharacter: 'Bender',
+//           tagline: expect.any(String),
+//           _id: expect.any(String)
+//         });
+//       });
+//   });
+// });
